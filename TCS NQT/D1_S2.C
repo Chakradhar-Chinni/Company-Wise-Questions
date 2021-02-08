ddcb993de0494086ded0a53fbda724cca605f562
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-02-08 11:40:43 +0530

    Create D1_S2.C

---
+++ TCS NQT/D1_S2.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	
+	if(n>=1 && n<=2000)
+	{
+		printf("Time Estimated : 25 minutes");
+	}
+	else if(n>=2000 && n<=4000)
+	{
+		printf("Time Estimated : 35 minutes");
+	}
+	else if(n>=4000 && n<=7000)
+	{
+		printf("Time Estimated : 45 minutes");
+	}
+	else if(n == 0)
+	{
+		printf("Time Estimated : O minutes");
+	}
+	else
+	{
+		printf("Invalid Output");
+	}
+}
